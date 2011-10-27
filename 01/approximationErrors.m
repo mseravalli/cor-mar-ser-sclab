d815d7c08fe94e5bb273701ab59b53a13f7449aa
Marco Seravalli <seravall@informatik.tu-muenchen.de>
2011-10-27 15:20:51 +0200

    added approximationErrors.m

---
+++ 01/approximationErrors.m
@@ -0,0 +1,29 @@
+function approxErrors = approximationErrors()
+%the first row will contain the error for the Euler approximation
+%the second row will contain the error for the Heun approximation
+%the third row will contain the error for the Runge-Kutta approximation
+%each column is a different (in this case smaller) dt
+	
+	y0 = 1;
+	tEnd = 10;
+
+	approxErrors = [];
+
+	for i = 0 : 3
+		dt = 1/(2.^i);
+	
+		approx = approxMethods(y0, dt, tEnd); 
+	
+		tmp = [];
+	
+		tmp = [tmp c(dt, approx(2,:), approx(1,:))];
+		tmp = [tmp c(dt, approx(3,:), approx(1,:))];
+		tmp = [tmp c(dt, approx(4,:), approx(1,:))];
+	
+		approxErrors = [approxErrors ; tmp];
+
+	end
+
+	approxErrors = approxErrors';
+
+end
