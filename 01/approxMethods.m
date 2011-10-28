df6e699889b75cd2cd32582c5c75400e94c08f9a
Marco Seravalli <marco.seravalli@gmail.com>
2011-10-28 13:41:35 +0200

    added labels to plots

--- 01/approxMethods.m
+++ 01/approxMethods.m
@@ -5,10 +5,15 @@ function approx = approxMethods(y0, dt, tEnd)
     fPrime = @(x)((1-x/10)  .*x);
     
     clf; 
-    hold on; 
+    hold on;
+	title('Approximation methods');
+	xlabel('t'); 
+	ylabel('p(t)');
     approx = a(dt, tEnd); 
     approx = [approx; b1(y0, dt, tEnd, fPrime)]; 
     approx = [approx; b2(y0, dt, tEnd, fPrime)]; 
     approx = [approx; b3(y0, dt, tEnd, fPrime)];
+	legend('exact', 'Euler', 'Heun', 'Runge-Kutta'); 
+	hold off;
 
 end
--- 01/plotApproxErr.m
+++ 01/plotApproxErr.m
@@ -1,10 +1,13 @@
 %script for plotting the approximation errors 
-%TODO label axes and curves
 approxErrors = approximationErrors();
 clf;
 hold on;
+title('Approximation errors');
+xlabel('dt');
+ylabel('Approximation error');
 plot([1/8, 1/4, 1/2, 1], fliplr(approxErrors(1,:)), 'r');
 plot([1/8, 1/4, 1/2, 1], fliplr(approxErrors(2,:)), 'g');
 plot([1/8, 1/4, 1/2, 1], fliplr(approxErrors(3,:)), 'black');
+legend('Euler', 'Heun', 'Runge-Kutta');
 hold off;
 clear approxErrors;
