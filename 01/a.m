29a0d873b30cf06429edb79f5e396ec4b6ca2c99
flaviocordero <flaviocordero@gmail.com>
2011-11-01 20:31:48 +0000

    function p = a(dt, tEnd) % p = a(dt, tEnd) % The function computes and plots the solutions for the equation p(t)=10./(1+9.*exp(-t)) in the range [0:dt:tEnd] % Returns p, the vector of values for each dt.
        t = [0:dt:tEnd];
        p=10 ./ (1 + 9.*exp(-t));
        plot(t, p);
    
    end

--- 01/a.m
+++ 01/a.m
@@ -1,6 +1,6 @@
 function p = a(dt, tEnd)
 % p = a(dt, tEnd)
-% The function computes and plots the solutions for the equation (1-10/p).*p in the range [0:dt:tEnd]
+% The function computes and plots the solutions for the equation p(t)=10./(1+9.*exp(-t)) in the range [0:dt:tEnd]
 % Returns p, the vector of values for each dt.
 
     t = [0:dt:tEnd];
