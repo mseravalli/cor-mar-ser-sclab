b60d8fdad5a770ae9bb49d9e5560729840804de5
Marco Seravalli <marco.seravalli@gmail.com>
2011-11-02 14:28:12 +0100

    legends added to plots

--- 01/runme.m
+++ 01/runme.m
@@ -46,11 +46,15 @@ title('Runge-Kutta method');
 xlabel('t'); 
 ylabel('p(t)');
 plot([0:0.1:tEnd], pExact, 'r');
+
+legendStr = ['Exact result'];
 	
 for j = 0 : maxPrec
 
 	dt = 1/(2^j);
 	tmp = approxMethods(y0, dt, tEnd);
+
+	legendStr = [legendStr ; ['dt = '  num2str(dt, '%1.2f') '   ']];
 
 	set(0,'CurrentFigure',eFig);
 	plot([0:dt:tEnd], tmp(2,:), 'Color', [0 1-(j/(maxPrec + 1)) j/(maxPrec + 1)]);
@@ -63,7 +67,40 @@ for j = 0 : maxPrec
 
 end
 
-clear appErr relErr errApp result i j;
+set(0,'CurrentFigure',eFig);
+legend(legendStr, 'Location','Best');
+
+set(0,'CurrentFigure',hFig);
+legend(legendStr, 'Location','Best');
+
+set(0,'CurrentFigure',rkFig);
+legend(legendStr, 'Location','Best');
+
+figure;
+plotApproxErr;
+
+
+clear appErr relErr errApp result i j dt tmp maxPrec;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
