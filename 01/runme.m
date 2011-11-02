b5f513dbcd0ca47c1fbfe9eb1979c808933bd443
Marco Seravalli <marco.seravalli@gmail.com>
2011-11-02 10:57:56 +0100

    runme added

---
+++ 01/runme.m
@@ -0,0 +1,21 @@
+appErr = approximationErrors;
+relErr = relativeErrors(approximationErrors);
+errApp = errorApproximation;
+titel = ['Euler      '; 'Heun       '; 'Runge-Kutta'];
+
+for i = 1 : 3
+
+	disp(titel(i,:))
+	result = [1 1/2 1/4 1/8];
+	result = [result ; appErr(i,:) ];	
+	result = [result ; [0/0 relErr(i,:)] ];	
+	result = [result ; errApp(i,:) ];	
+
+	disp(result)
+
+end
+
+clear appErr relErr errApp result i;
+
+
+
