4ce9055a1e06436dc229baa95f8af689e9f9ae6e
Marco Seravalli <marco.seravalli@gmail.com>
2011-11-09 13:53:12 +0100

    initialized folder 02 and added the explicit solvers

---
+++ 02/EHeun.m
@@ -0,0 +1,17 @@
+function p = EHeun(y0, dt, tEnd, fPrime)
+% p = b1(y0, dt, tEnd, fPrime) calculates the approximation for the given function using the Heun method.
+% Returns p, the vector of values for each dt.
+
+    p = [y0];    
+    tmp = y0;
+    
+    fPrimeTilde = @(x)( feval(fPrime , x + dt*feval(fPrime, x)));
+    
+    for t = dt : dt : tEnd
+                
+        tmp = tmp + 0.5.*dt .* (feval(fPrime, tmp)  + fPrimeTilde(tmp));
+        p = [p tmp];
+        
+    end
+
+end
---
+++ 02/ERK.m
@@ -0,0 +1,22 @@
+function p = ERK(y0, dt, tEnd, fPrime)
+% p = b1(y0, dt, tEnd, fPrime) calculates the approximation for the given function using the Runge-Kutta method.
+% Returns p, the vector of values for each dt.
+
+    p = [y0];    
+    tmp = y0;
+    
+    
+    fPrimeOne = fPrime;
+    fPrimeTwo = @(x)( fPrimeOne(x + 0.5.*dt*fPrimeOne(x)) );
+    fPrimeThree = @(x)(fPrimeOne(x + 0.5.*dt*fPrimeTwo(x)));
+    fPrimeFour = @(x)(fPrimeOne(x + dt*fPrimeThree(x)));
+    
+    
+    for t = dt : dt : tEnd
+                
+        tmp = tmp + 1/6.*dt .* (fPrimeOne(tmp)  + 2.*fPrimeTwo(tmp) + 2.*fPrimeThree(tmp) + fPrimeFour(tmp));
+        p = [p tmp];
+        
+    end
+
+end
