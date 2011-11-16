ad58420e326c39cd25f35461c64ece907692b43c
Marco Seravalli <marco.seravalli@gmail.com>
2011-11-16 12:13:31 +0100

    IEuler and AM cleaned

--- 02/IEuler.m
+++ 02/IEuler.m
@@ -3,21 +3,15 @@ function p = IEuler(y0, dt, tEnd, f, fPrime)
 % Returns p, the vector of values for each dt.
 
 	p = [y0];
-%	f      = @(x, x0)(x -  dt .* 7 .* (1 - (x/10)) .* x - x0);
-%	fPrime = @(x)(1 - dt .* 7 .* (1 -x/5));
-
-	disp('Implicit Euler');
-	
 
     for t = dt : dt : tEnd
-    	%if(y0 >= -5./14.*((1-7.*dt)^2./dt))
-	if((1-7*dt)^2 + 4*7*dt*y0/10 < 0)
-	     y0=inf;
-		disp('inf');
-		disp(dt);
-	else
-	     y0 = newtonMethod(y0, f, fPrime); 
-	end
+
+		if((1-7*dt)^2 + 4*7*dt*y0/10 < 0)
+	    	y0=inf;
+		else
+		     y0 = newtonMethod(y0, f, fPrime); 
+		end
+
 	p = [p y0]; 
         
     end
--- 02/adamsMoulton.m
+++ 02/adamsMoulton.m
@@ -3,16 +3,10 @@ function p = adamsMoulton(y0, dt, tEnd, f, fPrime)
 % Returns p, the vector of values for each dt.
 
 	p = [y0];
-%	f      = @(x, x0)(x - 0.5 .* dt .* 7 .*(  (1 - (x0/10)) .* x0 + (1 - (x/10)) .* x ) - x0);
-%	fPrime = @(x)(1 - dt .* 0.7 .* (5 -x));
     
-	disp('AM');
-
     for t = dt : dt : tEnd
        	if((1-(7.*dt)/2)^2 + 7.*dt/10 .* (2.*y0 + dt.*(7 .*(  (1 - (y0/10)) .* y0 )))  < 0)
 	    	y0 = inf;
-			disp('inf');
-			disp(dt);
 		else
 		    y0 = newtonMethod(y0, f, fPrime); 
 		end
