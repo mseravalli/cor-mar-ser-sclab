fa1f79e4689623ca1c05f285bbbb690a2338bac7
Marco Seravalli <marco.seravalli@gmail.com>
2011-11-21 09:43:46 +0100

    reintroduced the iterator in newton, in order to prevent bad behaviours with bad conditioned functions

--- 02/newtonMethod.m
+++ 02/newtonMethod.m
@@ -1,13 +1,25 @@
 function r = newtonMethod(x, f, fPrime)
+%root finding using newton's method
+%i required for ensuring termination even if the function is not well behaved
 	
-	x0 = x;
+    x0 = x;
 
-	while (abs( feval(f, x, x0 ) ) > 10.^(-4) )
+    i = 0; 
+
+    maxIter = 50;
+
+	while (i < maxIter && abs( feval(f, x, x0 ) ) > 10.^(-4) )
 	
 		x = x - feval(f, x, x0 ) ./ feval(fPrime, x);
 
-	end
-	
-	r = x;
+        i = i + 1;
 
+	end
+
+    if (i < 50)
+	    r = x;
+    else
+        r = Inf;
+    end
+	
 end
