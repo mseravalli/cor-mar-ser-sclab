851a63dbeb3cbbbdc1f049428d24aa91bfe29731
Milos Maric <maric@informatik.tu-muenchen.de>
2011-11-29 11:19:09 +0100

    now b) is in accordance with notation in worksheet

--- 03/b.m
+++ 03/b.m
@@ -1,4 +1,5 @@
 function mat = b(Nx, Ny)
+%mat = b(Nx, Ny) constructs coeficients matrix
 
 hx=1/(Nx+1);
 hy=1/	(Ny+1);
@@ -8,31 +9,31 @@ mat= zeros(Nx .* Ny, Nx .* Ny);
 i = 1;
 j = 1;
 
-for i = 1 : Nx
+for j = 1 : Ny
     
-	for j = 1 : Ny
+	for i = 1 : Nx
         
-        if i>1
-           mat((i-1)*Ny+j, (i-2)*Ny+j) = 1./(hy.^2);
+        if j>1
+           mat((j-1)*Nx+i, (j-2)*Nx+i) = 1./(hx.^2);
         end
 
-        if j-1>=1
-           mat((i-1)*Ny+j, (i-1)*Ny+j-1) = 1./(hx.^2);
+        if i-1>=1
+           mat((j-1)*Nx+i, (j-1)*Nx+i-1) = 1./(hy.^2);
         end
 
-        mat((i-1)*Ny+j, (i-1)*Ny+j) = -(2./(hx^2)+2./(hy^2));
+        mat((j-1)*Nx+i, (j-1)*Nx+i) = -(2./(hx^2)+2./(hy^2));
 
-        if j+1<=Ny
-           mat((i-1)*Ny+j, (i-1)*Ny+j+1) = 1./(hx.^2);
+        if i+1<=Nx
+           mat((j-1)*Nx+i, (j-1)*Nx+i+1) = 1./(hy.^2);
         end
 
-        if i<Nx
-            mat((i-1)*Ny+j, i*Ny+j) = 1./(hy.^2);
+        if j<Ny
+            mat((j-1)*Nx+i, j*Nx+i) = 1./(hx.^2);
         end
 
     end 
 
-    j = j + 1;
+    %i = i + 1;
 	
 end
 
